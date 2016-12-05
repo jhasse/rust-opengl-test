aa8b78bac4ae864d92c54d729aadd9045bde7419
Jan Niklas Hasse <jhasse@bixense.com>
2016-12-05 22:16:39 +0100

    Update crates and fix indention

--- src/modelview.rs
+++ src/modelview.rs
@@ -12,9 +12,9 @@ impl Modelview {
 
     pub fn translate(&mut self, x: f32, y: f32) {
         self.matrix = self.matrix * Matrix4::new(1f32, 0f32, 0f32, x,
-                                              0f32, 1f32, 0f32, y,
-                                              0f32, 0f32, 1f32, 0f32,
-                                              0f32, 0f32, 0f32, 1f32);
+                                                 0f32, 1f32, 0f32, y,
+                                                 0f32, 0f32, 1f32, 0f32,
+                                                 0f32, 0f32, 0f32, 1f32);
     }
 
     pub fn reset(&mut self) {
